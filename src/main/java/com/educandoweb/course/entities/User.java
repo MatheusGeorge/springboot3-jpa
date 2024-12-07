5f13a26be75293fe093aa32366cd0942d8a9e676
MatheusGeorge <matheus.vs0705@gmail.com>
2024-12-07 10:51:34 -0300

    Finalizando relacionamento de usuarios e ordens

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,6 +19,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
     @OneToMany(mappedBy = "client")
+    @JsonIgnore
     private List<Order> orders = new ArrayList<>();
 
     public User () {}
